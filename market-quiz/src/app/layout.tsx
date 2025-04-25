32be565578d3bcec8abbb10f76654b91199b0232
Michael <46937389+LastSpot@users.noreply.github.com>
2025-04-25 16:03:09 -0400

    refactor: migrate from Angular to Next.js framework
    - Removed Angular-specific files and configurations including components, services, and routing.
    - Added Next.js setup with updated package.json and scripts for development and production.
    - Updated README to reflect new project structure and usage instructions.
    - Configured TypeScript settings for Next.js and included necessary dependencies for React.
    - Removed unused files and configurations related to Angular, including .editorconfig and .vscode settings.

---
+++ market-quiz/src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import { inter } from '@/ui/fonts';
+
+export const metadata: Metadata = {
+  title: "Market Quiz",
+  description: "Market Quiz is a quiz game that tests your knowledge of market insights.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
